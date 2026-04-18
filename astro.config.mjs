6a08340ca71efe51ef21a420439ed84c9ff24166
pisec1-cpu <pisec1@gmail.com>
2026-04-18 14:43:30 +0300

    install metrika and robots txt and site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://lesstrsnab.ru',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
